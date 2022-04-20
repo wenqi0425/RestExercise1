f25431cabe4fb828104a70c3db30a2d4437ba9a2
Wenqi Huang <wenq0012@edu.easj.dk>
2022-04-20 19:40:06 +0200

    Add project files.

---
+++ RestExercise1/Controllers/ItemsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestExercise1.Managers;
+using RestExercise1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestExercise1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        //A object of the ItemsManager class
+        //Might be created several times, but the internal List is static
+        private ItemsManager _manager = new ItemsManager();
+
+        // GET: api/Items
+        //Gets all the items in the managers list
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/Items/5
+        //Gets a specific Item in the managers list, return null if the object wasn't found
+        //Notice the "{id}" part of the annotation, this makes the URI to the object expect a /int
+        [HttpGet("{id}")]
+        public Item Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/Items
+        //Sends the new object to list, which gives it a new ID and return the newly created object
+        //Notice the FromBody part of the parameter, this expects a Json body from the request
+        [HttpPost]
+        public Item Post([FromBody] Item newItem)
+        {
+            return _manager.Add(newItem);
+        }
+
+        // PUT api/Items/5
+        //Sends the id and the Item object to the manager to update the values
+        //The id in the object is ignored
+        //Returns null if no items has the id
+        //As seen here, we can mix different ways of expected data from the client
+        //Notice the "{id}" part of the annotation, this makes the URI to the object expect a /int
+        //Notice the FromBody part of the parameter, this expects a Json body from the request
+        [HttpPut("{id}")]
+        public Item Put(int id, [FromBody] Item updatedItem)
+        {
+            return _manager.Update(id, updatedItem);
+        }
+
+        // DELETE api/Items/5
+        //Asks the Manager to delete the item with the specific id
+        //Returns null if the item was not found
+        //Notice the "{id}" part of the annotation, this makes the URI to the object expect a /int
+        [HttpDelete("{id}")]
+        public Item Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ RestExercise1/Managers/ItemsManager.cs
@@ -0,0 +1,70 @@
+﻿using RestExercise1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestExercise1.Managers
+{
+    public class ItemsManager
+    {
+        //Keeps track of ids, in order for all items to have a unique ID
+        private static int _nextId = 1;
+        //Creates the list of Items and fills it with 3 items to begin with
+        //The 3 items is only for testing purposes
+        private static readonly List<Item> Data = new List<Item>
+        {
+            new Item {Id = _nextId++, Name = "Book about C#", ItemQuality = 300, Quantity = 10},
+            new Item {Id = _nextId++, Name = "Not a Book about C#", ItemQuality = 1, Quantity =1},
+            new Item {Id = _nextId++, Name = "Fruit basket", ItemQuality = 50, Quantity =5}
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        //Returns all items in the List, in a new List
+        public List<Item> GetAll()
+        {
+            return new List<Item>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        //Returns a specific Item from the list
+        //return null if the id is not found
+        public Item GetById(int id)
+        {
+            return Data.Find(item => item.Id == id);
+        }
+
+        //Adds a new Item to the list, and assigns it the next id
+        //returns the newly added Item
+        public Item Add(Item newItem)
+        {
+            newItem.Id = _nextId++;
+            Data.Add(newItem);
+            return newItem;
+        }
+
+        //Deletes the Item from the list with the specific Id
+        //Returns null of no Item has the Id
+        public Item Delete(int id)
+        {
+            Item item = Data.Find(item1 => item1.Id == id);
+            if (item == null) return null;
+            Data.Remove(item);
+            return item;
+        }
+
+        //Updates the values of the Item with the specific Id
+        //Returns null of no Item has the Id
+        //Notice Id is not changed in the Item from the List
+        public Item Update(int id, Item updates)
+        {
+            Item item = Data.Find(item1 => item1.Id == id);
+            if (item == null) return null;
+            item.Name = updates.Name;
+            item.ItemQuality = updates.ItemQuality;
+            item.Quantity = updates.Quantity;
+            return item;
+        }
+    }
+}
